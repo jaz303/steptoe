649076ba05720f9c43168abdd13cdab83025a36c
Jason Frame <jason@onehackoranother.com>
2014-04-01 17:22:07 +0100

    initial

---
+++ program.js
@@ -0,0 +1,23 @@
+var ast = require('./ast_nodes');
+
+var stmts = new ast.Statements();
+
+stmts.body.push(new ast.Assign(
+	new ast.Ident('a'),
+	new ast.BinOp(
+		new ast.Number(100),
+		'+',
+		new ast.Number(125)
+	)
+));
+
+stmts.body.push(new ast.Assign(
+	new ast.Ident('b'),
+	new ast.BinOp(
+		new ast.Number(50),
+		'-',
+		new ast.Number(30)
+	)
+));
+
+module.exports = stmts;

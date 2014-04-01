386a9116906e5f3f2d9ab8a3587a9d38e545704e
Jason Frame <jason@onehackoranother.com>
2014-04-01 17:26:29 +0100

    tabs -> spaces

--- ast_nodes.js
+++ ast_nodes.js
@@ -1,131 +1,131 @@
-exports.Statements 		= Statements;
-exports.Assign 			= Assign;
-exports.BinOp 			= BinOp;
-exports.Number			= NumberLiteral;
-exports.Ident 			= Ident;
+exports.Statements      = Statements;
+exports.Assign          = Assign;
+exports.BinOp           = BinOp;
+exports.Number          = NumberLiteral;
+exports.Ident           = Ident;
 
 var DEBUG = false;
 
 var nextid = 1;
 function genid(name) {
-	var id = DEBUG ? name : (nextid++);
-	exports[name] = id;
-	return id;
+    var id = DEBUG ? name : (nextid++);
+    exports[name] = id;
+    return id;
 }
 
-var STATEMENTS 	= genid('STATEMENTS'),
-	ASSIGN	 	= genid('ASSIGN'),
-	BIN_OP 		= genid('BIN_OP'),
-	NUMBER 		= genid('NUMBER'),
-	IDENT 		= genid('IDENT');
+var STATEMENTS  = genid('STATEMENTS'),
+    ASSIGN      = genid('ASSIGN'),
+    BIN_OP      = genid('BIN_OP'),
+    NUMBER      = genid('NUMBER'),
+    IDENT       = genid('IDENT');
 
 //
 // Statements
 
 function Statements(body) {
-	this.type = STATEMENTS;
-	this.body = body || [];
+    this.type = STATEMENTS;
+    this.body = body || [];
 }
 
 Statements.prototype.newFrame = function() {
-	return { ix: 0 };
+    return { ix: 0 };
 }
 
 Statements.prototype.step = function(state, env, ctx) {
-	if (state.ix >= this.body.length) {
-		ctx.ret(null);
-	} else {
-		ctx.evaluate(this.body[state.ix++]);
-	}
+    if (state.ix >= this.body.length) {
+        ctx.ret(null);
+    } else {
+        ctx.evaluate(this.body[state.ix++]);
+    }
 }
 
 //
 // Assign
 
 function Assign(left, right) {
-	this.left = left;
-	this.right = right;
+    this.left = left;
+    this.right = right;
 }
 
 Assign.prototype.type = ASSIGN;
 
 Assign.prototype.newFrame = function() {
-	return { state: 0 };
+    return { state: 0 };
 }
 
 Assign.prototype.step = function(state, env, ctx) {
-	if (state.state === 0) {
-		ctx.evaluate(this.right);
-	} else if (state.state === 1) {
-		env[this.left.name] = ctx.retVal;
-	} else {
-		ctx.ret(null);
-	}
-	state.state++;
+    if (state.state === 0) {
+        ctx.evaluate(this.right);
+    } else if (state.state === 1) {
+        env[this.left.name] = ctx.retVal;
+    } else {
+        ctx.ret(null);
+    }
+    state.state++;
 }
 
 //
 //
 
 function BinOp(left, op, right) {
-	this.type = BIN_OP;
-	this.left = left;
-	this.op = op;
-	this.right = right;
+    this.type = BIN_OP;
+    this.left = left;
+    this.op = op;
+    this.right = right;
 }
 
 BinOp.prototype.type = BIN_OP;
 
 BinOp.prototype.newFrame = function() {
-	return { state: 0, left: null };
+    return { state: 0, left: null };
 }
 
 BinOp.prototype.step = function(state, env, ctx) {
-	if (state.state === 0) {
-		ctx.evaluate(this.left);
-	} else if (state.state === 1) {
-		state.left = ctx.retVal;
-		ctx.evaluate(this.right);
-	} else {
-		if (this.op === '+') {
-			ctx.ret(state.left + ctx.retVal);
-		} else if (this.op === '-') {
-			ctx.ret(state.left - ctx.retVal);
-		} else {
-			throw new Error("runtime error - unsupported op!");
-		}
-	}
-	state.state++;
+    if (state.state === 0) {
+        ctx.evaluate(this.left);
+    } else if (state.state === 1) {
+        state.left = ctx.retVal;
+        ctx.evaluate(this.right);
+    } else {
+        if (this.op === '+') {
+            ctx.ret(state.left + ctx.retVal);
+        } else if (this.op === '-') {
+            ctx.ret(state.left - ctx.retVal);
+        } else {
+            throw new Error("runtime error - unsupported op!");
+        }
+    }
+    state.state++;
 }
 
 //
 //
 
 function NumberLiteral(value) {
-	this.type = NUMBER;
-	this.value = value;
+    this.type = NUMBER;
+    this.value = value;
 }
 
 NumberLiteral.prototype.newFrame = function() {
-	return null;
+    return null;
 }
 
 NumberLiteral.prototype.step = function(state, env, ctx) {
-	ctx.ret(this.value);
+    ctx.ret(this.value);
 }
 
 //
 //
 
 function Ident(name) {
-	this.type = IDENT;
-	this.name = name;
+    this.type = IDENT;
+    this.name = name;
 }
 
 Ident.prototype.newFrame = function() {
-	return null;
+    return null;
 }
 
 Ident.prototype.step = function(state, env, ctx) {
-	ctx.ret(env[this.name]);
+    ctx.ret(env[this.name]);
 }
--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-var prog 	= require('./program'),
-	A 		= require('./ast_nodes');
+var prog    = require('./program'),
+    A       = require('./ast_nodes');
 
 function Context() {
-	this.frames = [];
-	this.nodes = [];
-	this.env = {};
+    this.frames = [];
+    this.nodes = [];
+    this.env = {};
 }
 
 Context.prototype.ret = function(val) {
-	this.retVal = val;
-	this.frames.pop();
-	this.nodes.pop();
+    this.retVal = val;
+    this.frames.pop();
+    this.nodes.pop();
 }
 
 Context.prototype.evaluate = function(node) {
-	this.frames.push(node.newFrame());
-	this.nodes.push(node);
+    this.frames.push(node.newFrame());
+    this.nodes.push(node);
 }
 
 Context.prototype.step = function() {
-	this.nodes[this.nodes.length-1].step(
-		this.frames[this.frames.length-1],
-		this.env,
-		this
-	);
+    this.nodes[this.nodes.length-1].step(
+        this.frames[this.frames.length-1],
+        this.env,
+        this
+    );
 }
 
 var ctx = new Context();
 ctx.evaluate(prog);
 
 while (ctx.frames.length) {
-	console.log(ctx.frames[ctx.frames.length-1]);
-	ctx.step();
+    console.log(ctx.frames[ctx.frames.length-1]);
+    ctx.step();
 }
 
 console.log(ctx.env);
